16c105b70954ee59bfb55e8cfed8b597e1725b2c
André Reis <andre.reis@gmail.com>
2017-06-29 14:56:36 +0100

    Create useful_admin_queries.sql

---
+++ useful_admin_queries.sql
@@ -0,0 +1,5 @@
+-- List tables and filtered by schema, name , etc
+select * from information_schema.tables
+
+-- List sequences and filtered by schema, name , etc
+select * from information_schema.tables
